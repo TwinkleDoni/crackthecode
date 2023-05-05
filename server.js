b648542feb2ffc5e5c7b3dbcea8fd53d2bf616c9
TwinkleDoni <98153512+TwinkleDoni@users.noreply.github.com>
2023-05-05 07:44:03 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+
+const app = express(); 
+const port = 3000;
+
+
+
+app.use(express.static('./'));
+
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+
+  
+app.listen(port,() =>{
+    console.log(`Server is successfully running on port ${port}`);
+});
